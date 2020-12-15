2ec8bf7123d58a0b2fc8c740f286ae703b499e4b
Vladimir Sokolov <vsokol74@main.ru>
2020-12-15 20:43:04 +0300

    пустое приложение

---
+++ src/main/java/store/sokolov/checklist/ChecklistApplication.java
@@ -0,0 +1,13 @@
+package store.sokolov.checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChecklistApplication.class, args);
+    }
+
+}
